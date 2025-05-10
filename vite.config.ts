5ed3e669be1c2d6904a60def694a77cd01be3973
Toni Seas <toniseas.dev@gmail.com>
2025-05-09 22:12:53 -0600

    Add configuration to building the library

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,32 @@
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.tsx'),
+      name: 'EasyPDF',
+      fileName: (format) => `easy-pdf.${format}.js`,
+      formats: ['es', 'cjs', 'umd']
+    },
+    rollupOptions: {
+      // Externalize deps that shouldn't be bundled into the library
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  },
+  plugins: [react(), dts({ rollupTypes: true })],
 })
