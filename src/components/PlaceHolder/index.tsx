a00baf4c93cd674cce447c3d965cc4763eae45f6
Toni Seas <toniseas.dev@gmail.com>
2025-05-09 20:51:09 -0600

    Add initial component and tailwindcss

---
+++ src/components/PlaceHolder/index.tsx
@@ -0,0 +1,8 @@
+export const PlaceHolder = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <div className="w-1/2 h-1/2 bg-gray-200 rounded-lg animate-pulse"></div>
+      <div className="w-1/4 h-1/4 bg-gray-200 rounded-lg animate-pulse mt-4"></div>
+    </div>
+  )
+}
