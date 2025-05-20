e02f352f29c10f67d7dd1fa8d4251cacafbf98e5
Toni Seas <toniseas.dev@gmail.com>
2025-05-19 21:54:05 -0600

    style: update DocumentContainer to enhance styling and content

--- packages/react-pdf/src/components/DocumentContainer/index.tsx
+++ packages/react-pdf/src/components/DocumentContainer/index.tsx
@@ -5,5 +5,5 @@ interface DocumentContainerProps extends React.HTMLProps<HTMLDivElement> {
 }
 
 export const DocumentContainer: React.FC<DocumentContainerProps> = () => {
-  return <div className="flex">Test</div>;
+  return <div className="flex font-bold text-red-500 bg-amber-100">This is a test with style...</div>;
 };
