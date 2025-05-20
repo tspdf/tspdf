54e88d75f4ea2299e4cf7ff67dfd5bd4ec4fcf4a
Toni Seas <toniseas.dev@gmail.com>
2025-05-19 22:40:57 -0600

    chore: test pre-commit x2

--- packages/react-pdf/src/components/DocumentContainer/index.tsx
+++ packages/react-pdf/src/components/DocumentContainer/index.tsx
@@ -6,8 +6,14 @@ interface DocumentContainerProps extends React.HTMLProps<HTMLDivElement> {
 
 export const DocumentContainer: React.FC<DocumentContainerProps> = () => {
   return (
-    <div className="flex font-bold text-red-500 bg-amber-100">
-      This is a test with style....
+    <div
+    
+    className="flex font-bold text-red-500 bg-amber-100">
+      This is a test with style.....
+
+
+
+
     </div>
   );
 };
